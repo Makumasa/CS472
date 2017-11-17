100bfe4fd1f6349b74e61fee14e3080b66c20918
Makumasa <berezamark@gmail.com>
2017-11-17 07:14:20 -0800

    finished part 4 of assignment 3

--- assignment3/part4.c
+++ assignment3/part4.c
@@ -5,13 +5,18 @@ int main(int argc, char **argv)
     short val;
     char *p_val;
     p_val = (char *)&val;
-    /*
-    The following two lines assume big-endian
-    Architecture to initialize the variable Val
-    to 0x1234.
-    */
+
+#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+    printf("System uses big-endian memory architecture.\n");
     p_val[0] = 0x12;
     p_val[1] = 0x34;
+#elif __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+    printf("System uses little-endian memory architecture.\n");
+    p_val[1] = 0x12;
+    p_val[0] = 0x34;
+#else
+#error "Endianess cannot be determined at compile time."
+#endif
     printf("%x\n", val);
 
     return 0;
